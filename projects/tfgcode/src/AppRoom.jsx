9f6452488d6ae4fd0b71db357095b1ccee67d8c9
luismidv <perezmartluismiguel@gmail.com>
2024-11-23 12:33:15 +0100

    Improved frontend

--- projects/tfgcode/src/AppRoom.jsx
+++ projects/tfgcode/src/AppRoom.jsx
@@ -7,31 +7,31 @@ export function AppRoom(){
   return(
     <section id = 'room-container'>
       <RoomBanner label = "Room at the center of Valencia" img = "../../media1/roombanner1.webp" 
-      rooms = '4 ' metters = '120' bathrooms = '2' price = '350' bathroomsvg = "../../media1/RoomBanners/bathroom.svg"
+      rooms = '4 ' metters = '120' bathrooms = '2' price = '350 €' bathroomsvg = "../../media1/RoomBanners/bathroom.svg"
       bedroomsvg = "../../media1/RoomBanners/bedroom.svg"  eurosvg = "../../media1/RoomBanners/euro.svg" 
       profilesvg= "../../media1/RoomBanners/userprof.svg" username = "PepitoVendepisos"
-      directionsvg = "../../media1/RoomBanners/direction.svg" direction ="Joaquin Sorolla street"
+      directionsvg = "../../media1/RoomBanners/location.svg" direction ="Joaquin Sorolla street"
       description = "Beatiful house in the center "/>
 
       <RoomBanner label = "Room at the center of Madrid" img = "../../media1/roombanner2.jpg" 
-      rooms = '4' metters = '120' bathrooms = '2' price = '350' bathroomsvg = "../../media1/RoomBanners/bathroom.svg"
+      rooms = '4' metters = '120' bathrooms = '2' price = '350 €' bathroomsvg = "../../media1/RoomBanners/bathroom.svg"
       bedroomsvg = "../../media1/RoomBanners/bedroom.svg"  eurosvg = "../../media1/RoomBanners/euro.svg" 
       profilesvg= "../../media1/RoomBanners/userprof.svg" username = "PepitoVendepisos"
-      directionsvg = "../../media1/RoomBanners/direction.svg" direction="Joaquin Sorolla street"
+      directionsvg = "../../media1/RoomBanners/location.svg" direction="Joaquin Sorolla street"
       description = "Beatiful house in the center "/>
 
       <RoomBanner label = "Room at the center of Seville" img= "../../media1/roombanner3.webp" 
-      rooms = '4' metters = '120' bathrooms = '2' price = '350' bathroomsvg = "../../media1/RoomBanners/bathroom.svg"
+      rooms = '4' metters = '120' bathrooms = '2' price = '350 €' bathroomsvg = "../../media1/RoomBanners/bathroom.svg"
       bedroomsvg = "../../media1/RoomBanners/bedroom.svg"  eurosvg = "../../media1/RoomBanners/euro.svg" 
       profilesvg= "../../media1/RoomBanners/userprof.svg" username = "PepitoVendepisos"
-      directionsvg = "../../media1/RoomBanners/direction.svg" direction ="Joaquin Sorolla street"
+      directionsvg = "../../media1/RoomBanners/location.svg" direction ="Joaquin Sorolla street"
       description = "Beatiful house in the center "/>
       
       <RoomBanner label = "Room at the center of Malaga" img= "../../media1/roombanner4.jpg" 
-      rooms = '4' metters = '120' bathrooms = '2' price = '350' bathroomsvg = "../../media1/RoomBanners/bathroom.svg"
+      rooms = '4' metters = '120' bathrooms = '2' price = '350 €' bathroomsvg = "../../media1/RoomBanners/bathroom.svg"
       bedroomsvg = "../../media1/RoomBanners/bedroom.svg"  eurosvg = "../../media1/RoomBanners/euro.svg" 
       profilesvg= "../../media1/RoomBanners/userprof.svg" username = "PepitoVendepisos"
-      directionsvg = "../../media1/RoomBanners/direction.svg" direction ="Joaquin Sorolla street"
+      directionsvg = "../../media1/RoomBanners/location.svg" direction ="Joaquin Sorolla street"
       description = "Beatiful house in the center"
       />
     </section>
